9eeb367415bf22287af1ae9efc36b26124c8fb3e
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2016-12-30 13:09:26 +0700

    Extracted base object, API refactored

--- src/nimactors.nim
+++ src/nimactors.nim
@@ -4,42 +4,44 @@ import fp,
        os
 
 type
+  ActorCommand = enum amQuit
+  ActorBaseObj {.inheritable.} = object
+    name: string
+    cmdChannel: Channel[ActorCommand]
+    thread: Thread[ForeignCell]
+  ActorBase* = ref ActorBaseObj
+  ActorBasePtr* = ptr ActorBaseObj
   ActorHandlerS*[T,S] = proc(self: ActorPtr[T,S], msg: T, state: S): Option[S] {.gcsafe.}
   ActorHandler*[T] = proc(self: ActorPtr[T,Unit], msg: T): bool {.gcsafe.}
-  ActorMessageType = enum amQuit, amMsg
   ActorMessage[T] = object
-    case `type`: ActorMessageType
-    of amQuit:
-      discard
-    of amMsg:
-      msg: T
-      timeout: int
+    msg: T
+    timeout: int
   Actor*[T,S] = ref ActorObj[T,S]
   ActorPtr*[T,S] = ptr ActorObj[T,S]
-  ActorThreadArgs[T,S] = object
+  ActorThreadArgs[T,S] = ref object
     actor: ActorPtr[T,S]
     handler: ActorHandlerS[T,S]
-    initialState: S
-  ActorObj[T,S] = object of RootObj
+    initialstate: S
+  ActorObj[T,S] = object of ActorBaseObj
     ## The actor type. Actors are always typed
     channel: Channel[ActorMessage[T]]
     handler: ActorHandlerS[T,S]
-    name: string
-    thread: Thread[ActorThreadArgs[T,S]]
 
-proc mkAmQuit[T](): ActorMessage[T] = ActorMessage[T](`type`: amQuit)
-
-proc mkAmMsg[T](msg: T, timeout = 0): ActorMessage[T] = ActorMessage[T](`type`: amMsg, msg: msg, timeout: timeout)
+proc mkAmMsg[T](msg: T, timeout = 0): ActorMessage[T] = ActorMessage[T](msg: msg, timeout: timeout)
 
 template ActorPtrT*(T: untyped): untyped =
   var x: T
   type(x[].addr)
 
+proc initActorBase(actor: ActorBase, name: string) =
+  actor.cmdChannel.open
+  actor.name = ""
+
 proc initActor*[T,S](actor: var Actor[T,S], handler: ActorHandlerS[T,S]): ActorPtr[T,S] {.discardable, raises: [].} =
   new actor
+  initActorBase(actor, "")
   open(actor.channel)
   actor.handler = handler
-  actor.name = ""
   actor[].addr
 
 proc initActor*[T](actor: var Actor[T,Unit], handler: ActorHandler[T]): ActorPtr[T,Unit] {.discardable, raises: [].} =
@@ -62,7 +64,18 @@ proc setName*[T,S](a: Actor[T,S], name: string): Actor[T,S] {.discardable, raise
   a.name = name
   result = a
 
-proc actorThread[T,S](args: ActorThreadArgs[T,S]) {.thread, nimcall.} =
+proc handleCmd(cmd: ActorCommand): bool =
+  result = true
+  if cmd == amQuit:
+    result = false
+
+proc actorThread[T,S](fc: ForeignCell) {.thread, nimcall.} =
+  var args: ActorThreadArgs[T,S]
+  # TODO: https://github.com/nim-lang/Nim/issues/5166
+  let srcArgs = cast[ActorThreadArgs[T,S]](fc.data)
+  deepCopy(args, srcArgs)
+  fc.dispose
+  let cmdChannel = args.actor.cmdChannel.addr
   let channel = args.actor.channel.addr
   var state = args.initialState
   try:
@@ -74,13 +87,16 @@ proc actorThread[T,S](args: ActorThreadArgs[T,S]) {.thread, nimcall.} =
         poll(0)
       except:
         discard
-      var mmsg = channel[].tryRecv()
-      if mmsg.dataAvailable:
-        let am = mmsg.msg
-        case am.`type`
-        of amQuit:
+      block:
+        ## First we need to check command channel
+        var mmsg = cmdChannel[].tryRecv()
+        if mmsg.dataAvailable and not handleCmd(mmsg.msg):
           cont = false
-        of amMsg:
+      if cont:
+        ## Then we need to check messages channel
+        var mmsg = channel[].tryRecv()
+        if mmsg.dataAvailable:
+          let am = mmsg.msg
           if am.timeout <= 0:
             let so = args.handler(args.actor, am.msg, state)
             if so.isDefined:
@@ -104,21 +120,27 @@ proc actorThread[T,S](args: ActorThreadArgs[T,S]) {.thread, nimcall.} =
 
 proc start*[T,S](a: Actor[T,S], initialState: S): EitherS[Unit] =
   tryS do() -> auto:
-    a.thread.createThread(actorThread, ActorThreadArgs[T,S](actor: addr a[], handler: a.handler, initialState: initialState))
+    let args = ActorThreadArgs[T,S](actor: addr a[], handler: a.handler, initialState: initialState)
+    a.thread.createThread(actorThread[T,S], system.protect(cast[pointer](args)))
     ()
 
 proc start*[T](a: Actor[T,Unit]): EitherS[Unit] =
   a.start(())
+
+proc sendCmdImpl(a: ActorBase|ActorBasePtr, cmd: ActorCommand): EitherS[Unit] {.discardable.} =
+  tryS do -> auto:
+    a.cmdChannel.send(cmd)
+    ()
 
 proc sendImpl[T,S](a: Actor[T,S]|ActorPtr[T,S], msg: ActorMessage[T]): EitherS[Unit] {.discardable.} =
   tryS do -> auto:
     a.channel.send(msg)
     ()
 
-proc stop*[T,S](a: Actor[T,S]): EitherS[Unit] =
-  a.sendImpl(mkAmQuit[T]())
+proc stop*(a: ActorBase): EitherS[Unit] =
+  a.sendCmdImpl(amQuit)
 
-proc join*[T,S](a: Actor[T,S]): EitherS[Unit] =
+proc join*(a: ActorBase): EitherS[Unit] =
   tryS do() -> auto:
     a.thread.joinThread
     ()
@@ -131,3 +153,4 @@ proc `!`*[T,S](a: Actor[T,S]|ActorPtr[T,S], msg: T): EitherS[Unit] {.discardable
 
 proc sendDeferred*[T,S](a: Actor[T,S]|ActorPtr[T,S], msg: T, timeout: int): EitherS[Unit] {.discardable.} =
   a.sendImpl(mkAmMsg(msg, timeout))
+
